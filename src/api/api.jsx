c50418d6a1c0d44d960e15b9c571cc739c1c76b9
pra78 <66877961+pra78@users.noreply.github.com>
2023-04-30 12:34:51 +0300

    routes preparation

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,4 +1,8 @@
-const API_KEY = "63021acb907201a1471dfb5721049566";
-const API_URL = "https://api.themoviedb.org/3/movie/550?api_key=";
+import axios from "axios";
 
-const PATH = `${API_URL}${API_KEY}`;
+const refs = {
+    API_URL: "https://api.themoviedb.org/3/movie/550?api_key=",
+    API_KEY: "63021acb907201a1471dfb5721049566",
+}
+
+const PATH = `${refs.API_URL}${refs.API_KEY}`;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Cast from "../pages/Cast/Cast";
+import Home from "../pages/Home/Home";
+import MovieDetails from "../pages/MovieDetails/MovieDetails";
+import Movies from "../pages/Movies/Movies";
+import Reviews from "../pages/Reviews/Reviews";
+import NotFound from "pages/NotFound/NotFound";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
