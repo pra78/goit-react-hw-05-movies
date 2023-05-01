9da79915bc15bf0ff6696b4bf81b712d7f7bf2fa
pra78 <66877961+pra78@users.noreply.github.com>
2023-05-01 18:23:54 +0000

    Deploying to gh-pages from @ pra78/goit-react-hw-05-movies@15d032e32bdccb3954a19090d8aa204a9c91f87d 🚀

---
+++ static/js/256.d496843d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Not Found"}),(0,r.jsx)("p",{children:"Oops, something went wrong..."})]})}}}]);
+//# sourceMappingURL=256.d496843d.chunk.js.map
